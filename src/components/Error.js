2be71d13f21a9a7bc71e820a6b41df3fee100fc8
KirillV <78094438+KirillKirll@users.noreply.github.com>
2021-03-05 14:55:18 +0300

    Create Error.js

---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+import "../styles/tickets.css";
+import React from "react";
+
+export const Error = () => {
+  return (
+    <div className="NoTickets">Smth went wrong. Please reload the page</div>
+  );
+};
